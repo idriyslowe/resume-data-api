aed792755e100ab2d1a9f372de39efee79999569
Nina Leung <nleung320@gmail.com>
2015-12-20 15:14:35 -0600

    added skills seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Skill.create(skill_name: "Ruby on Rails", student_id: 1)
