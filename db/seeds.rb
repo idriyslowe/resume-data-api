4c76221f371e6d9ea6e82ff5d2fe885980eb89a7
Greg Bilodeau <gbil3478@gmail.com>
2015-12-20 15:14:53 -0600

    added experience seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# experience seed
+start_date: "2014-3-20", end_date: "2015-8-19", job_title: "the fucking boss", company: "acltc", details:"blah blah blah"}
