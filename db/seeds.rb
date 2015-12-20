3e6f730bf1d6eab1f447c7a4ee9bc262a45aab5e
Jamie <gatorjuice@gmail.com>
2015-12-20 15:23:21 -0600

    about to merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,5 @@ Student.create(
   github_url: "asldkjfalsdf",
   photo_url: "adljfhalsdfjsd"
 )
+Education.create(start_date: "2014-12-20", end_date: "2015-12-20", degree: "MBA", school: "Drop Zone Portable Toilets university", details: "This school taught me of all of my organizational skills and attention to detila.", student_id: 1)
+Capstone.create(name: "Mind Control App", description: "This app is the best I can come up with.", url: "https://github.com/idriyslowe/resume-data-api/tree/feature-ninaleung", screenshot_url: "https://upload.wikimedia.org/wikipedia/commons/2/24/Male_mallard_duck_2.jpg", student_id: 1)
