53fcef7cd7ade0ddf870271570dd040c9160ffd3
idriyslowe <idriyswins@gmail.com>
2015-12-20 14:33:06 -0600

    added association between education and student model

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,3 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151220201558) do
 
   create_table "educations", force: :cascade do |t|
@@ -23,8 +9,6 @@ ActiveRecord::Schema.define(version: 20151220201558) do
     t.integer  "student_id", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-=======
-ActiveRecord::Schema.define(version: 20151220201921) do
 
   create_table "capstones", force: :cascade do |t|
     t.string   "name",           limit: 255
@@ -34,7 +18,6 @@ ActiveRecord::Schema.define(version: 20151220201921) do
     t.integer  "student_id",     limit: 4
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
->>>>>>> 009eb2a98bee89d97766dd18807b2b76999bce76
   end
 
   create_table "experiences", force: :cascade do |t|
