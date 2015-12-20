0d237453dd62e036c3d4dd3f2a199c28829681a5
Nina Leung <nleung320@gmail.com>
2015-12-20 16:26:02 -0600

    generated students controller, actions, and view files

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,53 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all    
+  end
+
+  def show
+    @students = Student.find_by(id: params[:id])
+  end
+
+  def create
+    @student = Student.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter: params[:twitter],
+      blog_url: params[:blog_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url],
+      user_id: params[:user_id],
+      )
+    render :show
+  end
+
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter: params[:twitter],
+      blog_url: params[:blog_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url],
+      user_id: params[:user_id],
+      )
+    render :show
+  end
+
+  def destroy
+    @student = Student.find_by(id: params[:id])
+    @student.destroy
+    
+    @students = Student.all
+    render :index
+end
