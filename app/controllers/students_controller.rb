d46860954d801a3de7571327b00a4243581699d1
Jamie <gatorjuice@gmail.com>
2015-12-21 20:40:17 -0600

    we did stuff that made it work

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,62 +1,82 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all 
+    if params[:email] && params[:password]
+      user = Student.find_by(email: params[:email])
+      if user && params[:password] == user.password
+        @student = Student.find_by(email: params[:email])
+        render :show
+      else
+        render json: { valid: false }
+      end
+    elsif params[:id]
+      @student = Student.find_by(id: params[:id])
+      render :show
+    else
+      render json: { valid: false }
+    end 
   end
 
   def show
-    if params[:email] and params[:password] == Student.find_by(email: params[:email]).password
+   if params[:email] && params[:password]
+    user = Student.find_by(email: params[:email])
+    if user && params[:password] == user.password
       @student = Student.find_by(email: params[:email])
-    elsif params[:id]
-      @student = Student.find_by(id: params[:id])
+      render :show
     else
-      @student = {}
+      render json: { valid: false }
     end
-  end
-
-  def create
-    @student = Student.create(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone: params[:phone],
-      short_bio: params[:short_bio],
-      linkedin_url: params[:linkedin_url],
-      twitter: params[:twitter],
-      blog_url: params[:blog_url],
-      resume_url: params[:resume_url],
-      github_url: params[:github_url],
-      photo_url: params[:photo_url],
-      user_id: params[:user_id],
-      password: params[:password]
-      )
+  elsif params[:id]
+    @student = Student.find_by(id: params[:id])
     render :show
-  end
+  else
+    render json: { valid: false }
+  end 
+end
 
-  def update
-    @student = Student.find_by(email: params[:email])
-    @student.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone: params[:phone],
-      short_bio: params[:short_bio],
-      linkedin_url: params[:linkedin_url],
-      twitter: params[:twitter],
-      blog_url: params[:blog_url],
-      resume_url: params[:resume_url],
-      github_url: params[:github_url],
-      photo_url: params[:photo_url],
-      user_id: params[:user_id],
-      password: params[:password]
-      )
-    render :show
-  end
+def create
+  @student = Student.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    phone: params[:phone],
+    short_bio: params[:short_bio],
+    linkedin_url: params[:linkedin_url],
+    twitter: params[:twitter],
+    blog_url: params[:blog_url],
+    resume_url: params[:resume_url],
+    github_url: params[:github_url],
+    photo_url: params[:photo_url],
+    user_id: params[:user_id],
+    password: params[:password]
+    )
+  render :show
+end
 
-  def destroy
-    @student = Student.find_by(email: params[:email])
-    @student.destroy
-    
-    @students = Student.all
-    render :index
-  end
+def update
+  @student = Student.find_by(email: params[:email])
+  @student.update(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    phone: params[:phone],
+    short_bio: params[:short_bio],
+    linkedin_url: params[:linkedin_url],
+    twitter: params[:twitter],
+    blog_url: params[:blog_url],
+    resume_url: params[:resume_url],
+    github_url: params[:github_url],
+    photo_url: params[:photo_url],
+    user_id: params[:user_id],
+    password: params[:password]
+    )
+  render :show
+end
+
+def destroy
+  @student = Student.find_by(email: params[:email])
+  @student.destroy
+  
+  @students = Student.all
+  render :index
+end
 end
--- app/views/students/show.json.jbuilder
+++ app/views/students/show.json.jbuilder
@@ -1,3 +1,4 @@
+json.valid true
 json.id @student.id
 json.first_name @student.first_name
 json.last_name @student.last_name
