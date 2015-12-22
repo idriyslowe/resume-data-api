b5cce86856d13237659bb835dcc9f47507d208d2
idriyslowe <idriyswins@gmail.com>
2015-12-21 19:30:43 -0600

    added password to students

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220204658) do
+ActiveRecord::Schema.define(version: 20151222012725) do
 
   create_table "capstones", force: :cascade do |t|
     t.string   "name",           limit: 255
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20151220204658) do
     t.integer  "user_id",      limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "password",     limit: 255
   end
 
 end
